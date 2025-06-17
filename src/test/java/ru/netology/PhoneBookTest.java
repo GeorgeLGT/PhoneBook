6fd4eeb45aed64cd3566bbd0fa246cb24e904601
Georgy Lesyuk <rambozet@yandex.ru>
2025-06-17 22:23:31 +0300

    Stub and tests for add

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,7 +1,17 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
+
+    @Test
+    void testAddReturnsCount() {
+        PhoneBook pb = new PhoneBook();
+        assertEquals(1, pb.add("Alice", "12345"));
+        assertEquals(2, pb.add("Bob", "67890"));
+        assertEquals(2, pb.add("Alice", "54321"));
+    }
 
 }
